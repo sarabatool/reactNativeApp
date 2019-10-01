011e6e5ddbc3ab32fbd2f9260fd0957620c3b3dd
Sara Batool <sara.batool@tenpearls.com>
2019-10-01 15:24:08 +0500

    updated component name

--- ReactNativeApp/navigation/AppNavigator.js
+++ ReactNativeApp/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
+import MainTabNavigator from './HomeComponent';
 
 export default createAppContainer(
   createSwitchNavigator({
