f3c45543c8bbc07cb0603d1f733fde63321e12cd
Sara Batool <sara.batool@tenpearls.com>
2019-10-02 16:41:09 +0500

    WIP: Comment Screen

--- ReactNativeApp/screens/DishdetailComponent.js
+++ ReactNativeApp/screens/DishdetailComponent.js
@@ -1,74 +1,129 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, FlatList, View, ImageBackground, Button} from 'react-native';
+import {ScrollView, StyleSheet, Text, FlatList, View, ImageBackground, Button, Modal} from 'react-native';
 import * as leaders from '../shared/leaders';
-import {Card ,ListItem, Rating} from "react-native-elements";
+import {Card ,ListItem, Rating, Input} from "react-native-elements";
 
-export default function DishDetail() {
-    return (
-        <ScrollView style={styles.container}>
-            {History()}
-            {CorporateLeaderShip()}
-        </ScrollView>
-    );
-}
+class  DishDetail extends React.Component {
 
-const History =()=> {
-    return (
-        <Card style={{flex:1}}>
-            <ImageBackground
+    constructor(props){
+        super(props);
+        this.state = {
+            shouldShowModal: false
+        }
+    }
+    render() {
+        return (
+            <ScrollView style={styles.container}>
+                {this.History()}
+                {this.CorporateLeaderShip()}
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.shouldShowModal}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                    }}>
+                    <View style={{marginTop: 22}}>
+                        {this.addCommentModal()}
+                    </View>
+                </Modal>
+            </ScrollView>
+        );
+    }
 
-                source={require( '../assets/images/pizza_bg.jpg')}
-                style={styles.imgStyle}
-            >
-                <View style={styles.centerAlign}>
-                    <Text style={styles.textStyle}>Uthappizza</Text>
-                </View>
-            </ImageBackground>
-            <Text style={styles.regularText}>Uttapam (aka Uttappa or Oothapam) is yet another healthy breakfast recipe from South Indian cuisine prepared with a common rice and urad dal batter used for preparing Idli and dosa.
-            </Text>
 
-            <Button title='Add Comment' onPress={onAddComment}/>
-        </Card>
-    )
-};
+    History =()=> {
+        return (
+            <Card style={{flex:1}}>
+                <ImageBackground
 
-const onAddComment = () => {
- console.warn('Edit Comment');
-};
+                    source={require( '../assets/images/pizza_bg.jpg')}
+                    style={styles.imgStyle}
+                >
+                    <View style={styles.centerAlign}>
+                        <Text style={styles.textStyle}>Uthappizza</Text>
+                    </View>
+                </ImageBackground>
+                <Text style={styles.regularText}>Uttapam (aka Uttappa or Oothapam) is yet another healthy breakfast recipe from South Indian cuisine prepared with a common rice and urad dal batter used for preparing Idli and dosa.
+                </Text>
 
-const CorporateLeaderShip =() => {
-    return (
-        <Card title="Comments">
-            <FlatList
-                style={styles.list}
-                data={leaders.default.leadersList}
-                renderItem={({item,index}) => ( renderListItem(item,index))}
-                keyExtractor={item => item.id.toString()}
+                <Button title='Add Comment' onPress={this.toggleCommentModal}/>
+            </Card>
+        )
+    };
+
+    CorporateLeaderShip =() => {
+        return (
+            <Card title="Comments">
+                <FlatList
+                    style={styles.list}
+                    data={leaders.default.leadersList}
+                    renderItem={({item,index}) => ( this.renderListItem(item,index))}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Card>
+        )
+    };
+
+    toggleCommentModal = () => {
+        this.setState( prevState => ({
+            shouldShowModal: !prevState.shouldShowModal
+        }));
+
+    };
+
+    addCommentModal =() => {
+        return (
+            <View style={{margin: 20 }}>
+                <Rating
+                    style={{ paddingVertical: 10}}
+                    imageSize={20}
+                    showRating
+                />
+                <Input
+                    placeholder='Author'
+                    leftIcon={{ type: 'font-awesome', name: 'user' }}
+                />
+                <Input
+                    placeholder='Comment'
+                    leftIcon={{ type: 'font-awesome', name: 'comment' }}
+                />
+
+                <Button style={styles.submitBtn} title='Submit' onPress={console.warn('Submit Clicked')}/>
+                <Button title='Cancel' onPress={this.toggleCommentModal}/>
+        </View>
+        );
+
+    };
+
+    renderListItem = (item, index) => {
+        return (
+            <ListItem
+                key={index}
+                title={item.description}
+                titleStyle={styles.secondaryText}
+                subtitle={
+                    <View style={{margin: 0 }}>
+                        <Rating
+                            style={{flex:1,alignItems: 'flex-start', paddingVertical: 10}}
+                            readonly
+                            imageSize={20}
+                        />
+                        <Text style={styles.secondaryText}>{item.name}</Text>
+                    </View>
+                }
+                hideChevron={true}
             />
-        </Card>
-    )
-};
+        );
+    }
 
-const renderListItem = (item, index) => {
-    return (
-        <ListItem
-            key={index}
-            title={item.description}
-            titleStyle={styles.secondaryText}
-            subtitle={
-                <View style={{margin: 0 }}>
-                    <Rating
-                        style={{flex:1,alignItems: 'flex-start', paddingVertical: 10}}
-                        readonly
-                        imageSize={20}
-                    />
-                    <Text style={styles.secondaryText}>{item.name}</Text>
-                </View>
-            }
-            hideChevron={true}
-        />
-    );
 }
+
+export default DishDetail;
+
+
+
+
 
 DishDetail.navigationOptions = {
     title: 'Dish Detail',
@@ -153,6 +208,10 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    submitBtn: {
+        backgroundColor: 'red'
     }
+
 
 });
